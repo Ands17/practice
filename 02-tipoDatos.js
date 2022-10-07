448b4abaed9c0c5d9c7c74baea0cdb74a3d28997
Andrea Herrera <ands.he09@gmail.com>
2022-10-06 22:37:33 -0500

    first steps in js

---
+++ 02-tipoDatos.js
@@ -0,0 +1,10 @@
+//Datos no primitivosObjetos y arrays
+
+//Datos primitivos: no son objetos y no tienen métodos.
+//Todos los primitivos son inmutables, es decir, no se pueden modificar.
+/* 1. string
+2. number
+3. boleano
+4.null
+5. undefined
+6.  */
---
+++ 04alcanceDeVariables.js
@@ -0,0 +1,7 @@
+//Ambito  superglobal: contexto que engloba un archivo
+//Ambito global: Tenemos acceso desde cualquier lado del archivo
+/*variable esta libre /*
+//Ambito local: contexto que englaba a una funcion
+//ambito de bloque: engloba dentro de un bucle
+
+//SCOPE DE VARIABLE: NO RECONOCEVALOR DE LA VARIABLE FUERA DE LA FUNCION
