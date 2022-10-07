b5c9f484735ae7f59096da20b1526915e4c177c6
Andrea Herrera <ands.he09@gmail.com>
2022-10-06 22:44:45 -0500

    second class

---
+++ 05-arrays.js
@@ -0,0 +1,68 @@
+//array nos permite coleccionar datos
+//Métodos
+// var. en vsc nos muestra los metodos con morado y en azul las propiedades 
+
+//concat:Nos une 2 array o añade elementos al array
+
+//console.log(frutas.concat("limon"))
+
+//eVERY: obligs s que todos los elemnetos del array cumplan determinada condición
+
+//SOME: nos devuelve elemento por elemento si son true o false
+
+//frutas.Some(fruta=>{
+    //console.log(fruta===sandi)
+//})
+
+//FILTER: NOS FILTRA ARRAY Y NOS DEVUELVE OTRO ARRAY CON LAS CONDICIONES QUE LE INDIQUEMOS
+
+//FIND: NOS BUSCA LAS CONINCIDENCIAS QUE LE DIGAMOS
+
+//FOR EACH (POR CADA) POR CADA ELEMENTO NOS RECORRE ARAY
+
+//frutas.forEach(fruta=>{
+    //if(frutaa.length>5)
+    //{ console.log(fruta)}
+//})
+
+//includes: nos da true o false en  funcion si existe ese elemento del array
+
+//INDEXOF: nos devuelve un numero negativo si no encuentra el elemnto en el array
+//y si lo encuentra nos dice la posicion que ocupa
+
+//join: separa los elemntos del array segun le indiquemos
+
+// console.log(frutas.join("**"))
+
+
+//LASTINDEXOF_ LA ULTIMA COINCIDENCIA TE DA LA POSICION
+
+// PROPIEDADES
+
+//LENGTH: MIDE LONGITUD
+
+//MAP: NOS ITERA LOS ARRAY Y LOS ARRAY DE OBJETOS Y NOS DEVUEKVE UN ARRAY 
+
+//POP: elimina el último elemento de un array y lo devuelve
+//PUSh: AGREGA AL FINAL DEL ARRAY el nuevo elemento que le
+//indiquemos y nos devuelve la longitud con ese elemento
+
+//REDUCE: reduce al array a un solo elemento, al primero
+
+//reverse: nos devuelve el array invertido
+
+//slice: nos devuelve los indices comprendidos entre
+// el primer indice que le indiquemos y
+//el 2do no incluido(elemento anterior al indicado)
+
+//SORT: ordena por orden alfabeticamente
+
+//unshift: nos agrega al principio del array elemento que le indiquemos.
+
+//shift: nos elimina el primer elemento de array
+
+//splice: nos a elementos entre dos indices
+//1. ponrmos desde donde queremos que empiece a contar
+//2. cuantos elementos va a elimina o sumar
+//3. y si va a suma rlos que vaya a añadir
+
---
+++ 07.funciones-matematicas.js
@@ -0,0 +1,32 @@
+//valor absoluto
+
+console.log(Math.abs(4))
+
+//potencia
+console.log(Math.pow(2,3)) //2 elevado al 3)
+
+//redonder cifra al mas cercano
+//round(5.9938)
+
+console.log(Math.round(5.0985))
+
+//numero aleatorio8
+console.log(Math.random()*10)// no pasan del 10
+// sin descimales
+console.log(mat.round(Math.random()*10))
+
+//entre un rango de numeros, ni mas de 10 ni menos de 5
+console.log(Math.round(Math.random()*(10-5)+5))
+
+//si el numero es positivo o negativo
+//si nos da 1 es positivo
+
+console.log(math.sign(-45))
+
+//redonde hacia el entero superior
+//mat.ceill()
+
+//redonde hacia el entero inferior
+//math.floor(8.9)
+
+//
